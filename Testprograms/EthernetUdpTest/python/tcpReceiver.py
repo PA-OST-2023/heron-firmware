d8aef3c6ef652848b96ed522c15d64d939487068
Florian Baumgartner <florian.b-h@bluewin.ch>
2023-12-16 11:19:51 +0100

    Updated to latest state

---
+++ Testprograms/EthernetUdpTest/python/tcpReceiver.py
@@ -0,0 +1,54 @@
+# import socket
+
+# HOST = '192.168.33.10'  # The server's IP address
+# PORT = 6666             # The port used by the server
+
+# def main():
+#     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#         s.connect((HOST, PORT))
+#         print(f"Connected to {HOST}:{PORT}")
+
+#         try:
+#             while True:
+#                 data = s.recv(5)
+#                 if not data:
+#                     break
+#                 print(f"Received: {data.decode()}", end='')
+#         finally:
+#             print("\nClosing connection")
+#             s.close()
+
+# if __name__ == '__main__':
+#     main()
+
+
+
+
+
+import socket
+
+HOST = '192.168.33.10'  # The server's IP address
+PORT = 6666             # The port used by the server
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(b'\n')
+        print(f"Connected to {HOST}:{PORT}")
+
+        try:
+            while True:
+                # message = input("Enter message: ")
+                # if message.lower() == 'quit':
+                #     break
+
+                # s.sendall(b'\n')
+                data = s.recv(512)
+                # print(f"Received: {data.decode()}")
+
+        finally:
+            print("Closing connection")
+            s.close()
+
+if __name__ == '__main__':
+    main()
